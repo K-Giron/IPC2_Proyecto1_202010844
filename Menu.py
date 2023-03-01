e2c5af1d5aaba13bf8ad05995b1b6cfd0f4b7c42
K-Giron <3611696550101@ingenieria.usac.edu.gt>
2023-03-01 17:36:45 -0600

    Creando clases

---
+++ Menu.py
@@ -0,0 +1,15 @@
+
+class Menu:
+    def __init__(self) -> None:
+        self.opciones=[
+            'Abrir muestra',
+            'Graficar muestra',
+            '----',
+            '----',
+            '----',
+            'Acerca de',
+            'Salir'
+        ]
+
+    def prueba(self):
+        print("Hola")
